724d5b8cee16dbf0b4e076df5767c0b53da63344
Maxim Petrov <mail@maximpetrov.ru>
2025-03-21 14:05:08 +0300

    Update

--- src/bottom-sheet/tokens/utils/tokenUtils.js
+++ src/bottom-sheet/tokens/utils/tokenUtils.js
@@ -24,23 +24,6 @@ const parseTokenValue = (value) => {
 
 // Processed tokens generated from component/tokens.json
 const processedTokens = {
-  "THUMB_TRANSITION_DURATION": "300ms",
-  "THUMB_TRANSITION_EASING": "cubic-bezier(0.25, 0.1, 0.25, 1)",
-  "THUMB_HOVER_DURATION": "300ms",
-  "THUMB_DRAG_DURATION": "200ms",
-  "THUMB_DRAG_EASING": "cubic-bezier(.165, .84, .44, 1)",
-  "THUMB_DOT_EXPAND_DURATION": "300ms",
-  "THUMB_DOT_COLLAPSE_DURATION": "300ms",
-  "THUMB_DOT_TRANSITION_EASING": "cubic-bezier(.165, .84, .44, 1)",
-  "AXIS_TRANSITION_DURATION": "200ms",
-  "AXIS_TRANSITION_EASING": "cubic-bezier(0, 0, 1, 1)",
-  "AXIS_FILL_TRANSITION_DURATION": "200ms",
-  "AXIS_FILL_ACTIVE_DURATION": 0,
-  "COUNTER_TRANSITION_DURATION": "100ms",
-  "COUNTER_TRANSITION_EASING": "cubic-bezier(0, 0, 1, 1)",
-  "SLIDER_ANIMATION_DURATION": "500ms",
-  "SLIDER_TRANSITION_DURATION": "300ms",
-  "SLIDER_TRANSITION_EASING": "cubic-bezier(.165, .84, .44, 1)",
   "BOTTOM_SHEET_EXIT_DURATION": "300ms",
   "BOTTOM_SHEET_EXIT_EASING": "cubic-bezier(.165, .84, .44, 1)",
   "BOTTOM_SHEET_OVERLAY_ENTER_DURATION": "300ms",
